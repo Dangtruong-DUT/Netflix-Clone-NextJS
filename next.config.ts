a5d27c0c1c63d76c05ebaab5c02c0f3ece80f17a
Truong Nguyen <ndtrg281@gmail.com>
2025-09-16 19:41:30 +0700

    feat: add rewrites for homepage redirection and route base implement

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
-    /* config options here */
+    async rewrites() {
+        return [
+            {
+                source: "/",
+                destination: "/home",
+            },
+        ];
+    },
 };
 
 const withNextIntl = createNextIntlPlugin();
--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { ThemeProvider } from "@/components/theme-provider";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { hasLocale } from "next-intl";
 import { notFound } from "next/navigation";
@@ -8,16 +7,6 @@ import { routing } from "@/i18n/routing";
 import { setRequestLocale } from "next-intl/server";
 import { NetflixIcons, NetflixSans } from "@/config/font";
 import { NextIntlClientProvider } from "next-intl";
-
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
     title: "Create Next App",
